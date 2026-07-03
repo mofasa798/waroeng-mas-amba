bc1557d3433dc2aa0c31a696eb29f4aff0244f6c
mofasa798 <f.aji2803@gmail.com>
2026-07-03 11:03:35 +0700

    fix: Update API and frontend URLs to use port 8080

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    $frontend = env('FRONTEND_URL', 'http://localhost:3000');
+    $frontend = env('FRONTEND_URL', 'http://localhost:8080');
     return redirect($frontend . '/login');
 });
