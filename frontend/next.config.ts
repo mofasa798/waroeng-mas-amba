d4c4c61fe6518a1cad4d12a73377e0acc0dbcca1
mofasa798 <f.aji2803@gmail.com>
2026-07-03 13:10:26 +0700

    fix: Update CORS configuration to allow all origins and disable credentials

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  allowedDevOrigins: ["waroeng-mas-amba.test"],
 };
 
 export default nextConfig;
